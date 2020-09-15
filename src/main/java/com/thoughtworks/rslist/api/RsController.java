4342d0c097dcf9143afd4ea381513b0ff5a6512b
dai-yu <1435480344@qq.com>
2020-09-15 23:09:31 +0800

    feat:test for modify a news has name but type

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,50 @@
 package com.thoughtworks.rslist.api;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.thoughtworks.rslist.domain.News;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<News> rsList = init();
+
+  private List<News> init() {
+    List<News> rsList=new ArrayList<>();
+    rsList.add(new News("第一件事","无标签"));
+    rsList.add(new News("第二件事","无标签"));
+    rsList.add(new News("第三件事","无标签"));
+    return rsList;
+  }
+
+  @GetMapping("/rs/list")
+  public List<News> list(@RequestParam(required = false)Integer start, @RequestParam(required = false)Integer end){
+    if(start==null || end==null){
+      return rsList;
+    }
+    return rsList.subList(start-1,end);
+  }
+
+
+  @GetMapping("/rs/{index}")
+  public News oneNews(@PathVariable int index){
+    return rsList.get(index-1);
+  }
+
+  @PostMapping("/rs/add")
+  public void add(@RequestBody News news){
+    rsList.add(news);
+  }
+
+  @PutMapping("/rs/modify")
+  public void modify(@RequestParam Integer index,@RequestBody News news){
+    if(news.getName()==""){
+      news.setName(rsList.get(index-1).getName());
+    }
+    if(news.getType()==""){
+      news.setType(rsList.get(index-1).getType());
+    }
+    rsList.set(index-1,news);
+  }
 }
---
+++ src/main/java/com/thoughtworks/rslist/domain/News.java
@@ -0,0 +1,30 @@
+package com.thoughtworks.rslist.domain;
+
+public class News {
+    private String name;
+    private String type;
+
+    public News(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+    public News(){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
