57277568cd7b16cb1a1c6588870689221468bcdd
dai-yu <1435480344@qq.com>
2020-09-15 21:11:44 +0800

    test

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -1,11 +1,28 @@
 package com.thoughtworks.rslist.api;
 
+import com.thoughtworks.rslist.domain.News;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class RsController {
-  private List<String> rsList = Arrays.asList("第一条事件", "第二条事件", "第三条事件");
+  private List<News> rsList = init();
+
+  private List<News> init() {
+    List<News> rsList=new ArrayList<>();
+    rsList.add(new News("第一件事","无标签"));
+    rsList.add(new News("第二件事","无标签"));
+    rsList.add(new News("第三件事","无标签"));
+    return rsList;
+  }
+
+  @GetMapping("rs/list")
+  public List<News> list(){
+    return rsList;
+  }
 }
--- src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
+++ src/test/java/com/thoughtworks/rslist/RsListApplicationTests.java
@@ -1,13 +1,34 @@
 package com.thoughtworks.rslist;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.hamcrest.Matchers.is;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class RsListApplicationTests {
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mockMvc.perform(get("/rs/list")).andExpect(jsonPath("$",hasSize(3)))
+                .andExpect(jsonPath("$[0].name",is("第一件事")))
+                .andExpect(jsonPath("$[0].type",is("无标签")))
+                .andExpect(jsonPath("$[1].name",is("第二件事")))
+                .andExpect(jsonPath("$[1].type",is("无标签")))
+                .andExpect(jsonPath("$[2].name",is("第三件事")))
+                .andExpect(jsonPath("$[2].type",is("无标签")))
+                .andExpect(status().isOk());
     }
+
 
 }
