dc697c6ecc021d29ba55b66c1c381c4b15ceb226
dai-yu <1435480344@qq.com>
2020-09-19 22:59:16 +0800

    feat:should get votes for a specified time period.

--- src/main/java/com/thoughtworks/rslist/repository/VoteRepository.java
+++ src/main/java/com/thoughtworks/rslist/repository/VoteRepository.java
@@ -2,6 +2,7 @@ package com.thoughtworks.rslist.repository;
 
 import com.thoughtworks.rslist.po.UserPO;
 import com.thoughtworks.rslist.po.VotePO;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -11,4 +12,7 @@ public interface VoteRepository extends CrudRepository<VotePO,Integer> {
     List<VotePO> findAll();
 
     VotePO findByUserPO(UserPO userPO);
+
+    @Query(value = "select * from Vote where vote_time between :startTime and :endTime",nativeQuery = true)
+    List<VotePO> findAllByDate(String startTime, String endTime);
 }
